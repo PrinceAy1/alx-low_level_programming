f5cb89c4584ede0a3410450683812ba97a26b864
PrinceAy1 <102287613+PrinceAy1@users.noreply.github.com>
2022-05-02 22:27:48 +0100

    Update 1-memcpy.c

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,17 +1,22 @@
 #include "main.h"
 /**
- * _memcpy - copies memory area,
- * @dest: destination memory area.
- * @src: source memory area.
- * @n: bytes filled.
- * Return: the pointer to dest.
+ * _memcpy - This is my funcion copy Src to dest
+ * @dest: This is my result
+ * @src: This is my string
+ * @n: This is n my number bytes to copy
+ *
+ * Return: This my my string copy
  */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	unsigned int a;
+	char *pDst = dest;
+	char const *pSrc =  src;
 
-	for (i = 0; i < n; i++)
-		*(dest + i) =  *(src + i);
-
+	for (a = 0; a < n; a++)
+	{
+		*pDst++ = *pSrc++;
+	}
 	return (dest);
 }
