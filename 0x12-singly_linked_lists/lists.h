41b8711ab4cc785ea2caaac75dab1c6bcb5d4405
PrinceAy1 <102287613+PrinceAy1@users.noreply.github.com>
2022-05-25 03:10:19 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,10 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LINKED_H
+#define _LINKED_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
-#include <string.h>
+#include <stdio.h>
+
+int _putchar(char c);
 
 /**
  * struct list_s - singly linked list
@@ -17,9 +17,9 @@
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -27,6 +27,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void before_main(void)__attribute__((constructor));
+void myStartupFun(void) __attribute__ ((constructor));
+void myConstructor(void);
 
 #endif
