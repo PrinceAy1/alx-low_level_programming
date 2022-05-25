2f9b9d76f91188061fcf4f9fa33fac9e6f206022
PrinceAy1 <102287613+PrinceAy1@users.noreply.github.com>
2022-05-25 03:19:25 +0100

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,23 +1,22 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * free_list - Function that frees a list
- * @head: points to a pointer to the singly linked list created
- * Return: Nothing
+ *free_list - function to free list
+ *@head: a pointer to a head
+ *
+ *Return: Void
  */
 
 void free_list(list_t *head)
 {
-	list_t *nextNode, *Node;
+list_t *tmp;
+tmp = head;
 
-	Node = head;
-	while (Node)
-	{	nextNode = Node->next;
-		free(Node->str);
-		free(Node);
-		Node = nextNode;
-	}
+while (head != NULL)
+{
+tmp = head;
+head = tmp->next;
+free(tmp->str);
+free(tmp);
+}
 }
