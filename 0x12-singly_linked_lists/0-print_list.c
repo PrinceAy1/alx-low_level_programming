640ab36f7b251e2150c36888f85164709927e75e
PrinceAy1 <102287613+PrinceAy1@users.noreply.github.com>
2022-05-25 03:12:21 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,37 +1,31 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * print_list - Function that prints all the elements of a singly linked list
- * @h: points to the singly linked list created
- * Return:The number of nodes
+ *print_list - linked list to print
+ *@h: pointer to head
+ *
+ *Return: number of nodes
  */
 
 size_t print_list(const list_t *h)
 {
-	size_t numberNode = 0;
-	int start = 1;
-	list_t *forwardPtr;
+unsigned int count = 0;
+const list_t *tmp;
 
-	forwardPtr = malloc(sizeof(list_t));
-	if (forwardPtr == NULL || h == NULL)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	while (start)
-	{
-	numberNode++;
-	if (h->str != NULL)
-		printf("[%d] %s\n", h->len, h->str);
-	else
-		printf("[0] (nil)\n");
-	if (h->next == NULL)
-		start = 0;
-	forwardPtr = h->next;
-	h = forwardPtr;
-	}
-	free(forwardPtr);
-	return (numberNode);
+tmp = h;
+
+while (tmp != NULL)
+{
+if (tmp->str)
+{
+printf("[%u] %s\n", tmp->len, tmp->str);
+}
+else
+{
+printf("[%u] %s\n", 0, "(nil)");
+}
+tmp = tmp->next;
+count++;
+}
+return (count);
 }
